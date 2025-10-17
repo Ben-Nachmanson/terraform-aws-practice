211200ce107044da80cffd4450853c5ea6fa98ad
Ben Nachmanson <bennachmanson@gmail.com>
2025-10-17 08:40:26 -0500

    Refactor to use ASG and launch template, add app_port variable
    Replaces standalone EC2 instance with an Auto Scaling Group and launch template for improved scalability. Adds data sources for default VPC and subnets, introduces the 'app_port' variable for configurable HTTP server port, and updates security group and user data to use this variable.

--- main.tf
+++ main.tf
@@ -2,6 +2,23 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+variable "app_port" {
+  description = "Port for the demo HTTP server to listen on"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
@@ -15,8 +32,8 @@ resource "aws_security_group" "instance" {
 
   # HTTP test page
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.app_port
+    to_port     = var.app_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -30,20 +47,40 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-077b630ef539aa0b5"
-  instance_type          = "t3.micro"
-  key_name               = "my-key"
+resource "aws_launch_template" "example" {
+  name_prefix   = "terraform-example-"
+  image_id      = "ami-077b630ef539aa0b5"
+  instance_type = "t3.micro"
+  key_name      = "my-key"  
   vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
+  user_data = base64encode(<<-EOF
     #!/bin/bash
     cd /home/ec2-user
     echo "Hello, World" > index.html
-    nohup python3 -m http.server 8080 &
+    python3 -m http.server ${var.app_port} &
   EOF
+  )
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  tags = {
-    Name = "terraform-example"
+resource "aws_autoscaling_group" "example" {
+  desired_capacity    = 2
+  min_size            = 2
+  max_size            = 10
+  vpc_zone_identifier = data.aws_subnets.default.ids
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
